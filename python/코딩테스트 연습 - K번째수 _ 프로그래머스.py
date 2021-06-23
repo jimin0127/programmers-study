496b5504bc7bdc6ffc53226e17952749533ee667
jimin0127 <ckdgh4662@naver.com>
2021-06-23 23:06:03 +0900

    K번째수

---
+++ python/코딩테스트 연습 - K번째수 _ 프로그래머스.py
@@ -0,0 +1,6 @@
+def solution(array, commands):
+    answer = []
+    for command in commands : 
+        list = array[command[0]-1:command[1]].sort()
+        answer.append(list[command[2]-1])
+    return answer
