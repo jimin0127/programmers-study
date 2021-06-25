7092f98e2ae5b8dc3a0143130f11ecf27959beb7
jimin0127 <ckdgh4662@naver.com>
2021-06-25 22:04:40 +0900

    정수 내림차순으로 배치하기

---
+++ 코딩테스트 연습 - 정수 내림차순으로 배치하기 _ 프로그래머스.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class Solution {
+    public long solution(long n) {
+        String[] arr=String.valueOf(n).split("");
+
+        Arrays.sort(arr);
+        String str = "";
+
+        for(int i=arr.length-1; i>=0; i--){
+             str+= arr[i];
+        }
+
+        return Long.parseLong(str);
+
+    }
+}
